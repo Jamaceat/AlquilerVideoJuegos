4b8e5ccad1b8a9ee00e5d0b6cb604fdb979c5365
Johan <jamacea@uninorte.edu.co>
2022-10-27 00:59:19 -0500

    Inicializando proyecto

---
+++ Front/rentgames/src/App.js
@@ -0,0 +1,11 @@
+import OptionBar from "./components/OptionBar"
+
+function App() {
+	return (
+		<>
+			<OptionBar />
+		</>
+	)
+}
+
+export default App
---
+++ Front/rentgames/src/components/OptionBar.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Barra() {
+	return <div></div>
+}
+
+export default Barra
