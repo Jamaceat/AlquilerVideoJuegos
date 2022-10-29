d1ce96c8ec904b06e98af8942b661dc3874b3dc2
Johan <jamacea@uninorte.edu.co>
2022-10-29 11:17:39 -0500

    routes

--- Front/rentgames/src/App.js
+++ Front/rentgames/src/App.js
@@ -4,14 +4,17 @@ import React from "react"
 import Header from "./components/Header/header"
 import Welcome from "./components/Welcome/Welcome"
 import Bills from "./components/Bills/Bills"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Header />
-			<Welcome />
-			<Bills />
-		</>
+			<Routes>
+				<Route exact path="/" element={<Welcome />} />
+				<Route exact path="/Bills" element={<Bills />} />
+			</Routes>
+		</Router>
 	)
 }
 
--- Front/rentgames/src/components/Header/header.js
+++ Front/rentgames/src/components/Header/header.js
@@ -2,15 +2,18 @@ import React from "react"
 import {AppBar, Toolbar, IconButton, Link} from "@mui/material"
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports"
 import "./header.css"
+import {Link as RouterLink} from "react-router-dom"
 
 function Header() {
 	return (
 		<AppBar position="static">
-			<Toolbar sx={{bgcolor: "#000000"}}>
-				<IconButton aria-label="Logoicon">
-					<h6 className="textologo white">RentGames</h6>
-					<SportsEsportsIcon sx={{color: "white"}} />
-				</IconButton>
+			<Toolbar className="shadow" sx={{bgcolor: "#000000"}}>
+				<Link component={RouterLink} to="/" underline="none">
+					<IconButton aria-label="Logoicon">
+						<h6 className="textologo white">RentGames</h6>
+						<SportsEsportsIcon sx={{color: "white"}} />
+					</IconButton>
+				</Link>
 			</Toolbar>
 		</AppBar>
 	)
