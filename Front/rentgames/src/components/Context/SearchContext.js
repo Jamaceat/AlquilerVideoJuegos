9a3947c2d0050e58eeed85da30064bc9f5e1a4a2
Johan <jamacea@uninorte.edu.co>
2022-10-30 12:30:24 -0500

    All features finished

--- Front/rentgames/src/components/Context/SearchContext.js
+++ Front/rentgames/src/components/Context/SearchContext.js
@@ -3,6 +3,13 @@ import {useState} from "react"
 export default function SearchContext() {
 	const [tipo, setTipo] = useState("ReleaseDate")
 	const [valorActual, setValorActual] = useState("")
+	const [datos, setDatos] = useState([])
+
+	// Agrega los datos que seran las filas de la tabla
+
+	const addDatos = (data) => {
+		setDatos(data)
+	}
 
 	// Valores posibles= 'title':String ,ReleaseDate:int , protagonist:string ,director:string
 	const addTipo = (data) => {
@@ -14,5 +21,5 @@ export default function SearchContext() {
 	}
 	//  Retorna el tipo que debe recibir en los inputs dependiendo del hook tipo
 
-	return {tipo, valorActual, addTipo, addValorActual}
+	return {tipo, valorActual, datos, addDatos, addTipo, addValorActual}
 }
--- Front/rentgames/src/components/Games/Search/Search.js
+++ Front/rentgames/src/components/Games/Search/Search.js
@@ -1,11 +1,8 @@
 import {
-	Container,
 	Box,
 	FormControl,
 	Input,
 	InputLabel,
-	FormHelperText,
-	Button,
 	MenuItem,
 	Select,
 } from "@mui/material"
@@ -13,22 +10,67 @@ import React from "react"
 import {useRef} from "react"
 import SearchContext from "../../Context/SearchContext"
 import {useEffect} from "react"
+import "./Search.css"
+import axios from "axios"
+import {DataGrid} from "@mui/x-data-grid"
 
 const listaTipos = {
-	ReleaseDate: "yyyy-mm-dd",
+	ReleaseDate: "yyyy",
 	title: "Call Of Duty",
 	protagonist: "CJ",
 	director: "EA",
 }
 
+const columns = [
+	{field: "idGame", headerName: "ID Game", width: 70},
+	{field: "name", headerName: "Game Title", width: 250},
+	{field: "releaseYear", headerName: "Release Year", width: 100},
+	{field: "protagonist", headerName: "Protagonist", width: 150},
+	{field: "director", headerName: "Director", width: 150},
+	{field: "platform", headerName: "Platforms", width: 200},
+	{field: "rentalPrice", headerName: "Rental Price", width: 150},
+]
+
 export default function SearchGame() {
-	const {tipo, valorActual, addValorActual, addTipo} = SearchContext()
+	const {tipo, valorActual, datos, addDatos, addValorActual, addTipo} =
+		SearchContext()
 
 	const textRef = useRef()
 	// const textTipo = useRef()
 
 	const textRefFu = () => {
-		addValorActual(textRef.current.firstChild.value)
+		const value = textRef.current.firstChild.value
+		addValorActual(value)
+		// addValorActual(value)
+		// console.log(valorActual, "valorActual")
+		// console.log(datos)
+	}
+
+	useEffect(() => {
+		valorActual === "" ? allData() : Busqueda()
+	}, [valorActual])
+
+	const allData = async () => {
+		try {
+			const response = await axios.get("https://localhost:44315/api/Games")
+			const {data} = response
+			addDatos(data)
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	const Busqueda = async () => {
+		try {
+			const nuevoTexto = valorActual.split(" ").join("%20")
+			const response = await axios.get(
+				`https://localhost:44315/api/Games/InfoGames/${tipo}/${nuevoTexto}`
+			)
+			const {data} = response
+			addDatos(data)
+		} catch (e) {
+			console.log(e)
+		}
 	}
 
 	// useEffect(() => {
@@ -36,50 +78,67 @@ export default function SearchGame() {
 	// })
 
 	return (
-		<>
-			{tipo === "ReleaseDate" ? (
-				<FormControl fullWidth>
-					<InputLabel>Date</InputLabel>
-					<Input
-						placeholder={listaTipos[tipo]}
-						id="precio"
-						required={true}
-						onChange={textRefFu}
-						autoComplete="off"
-						type="number"
-						ref={textRef}
+		<div>
+			<div className="searchCont">
+				<div className="formulario">
+					{tipo === "ReleaseDate" ? (
+						<FormControl fullWidth>
+							<InputLabel>Date</InputLabel>
+							<Input
+								placeholder={listaTipos[tipo]}
+								id="precio"
+								required={true}
+								onChange={textRefFu}
+								autoComplete="off"
+								type="number"
+								ref={textRef}
+							/>
+						</FormControl>
+					) : (
+						<FormControl onChange={textRefFu} fullWidth>
+							<InputLabel>
+								{tipo[0].toUpperCase() + tipo.substring(1)}
+							</InputLabel>
+							<Input
+								placeholder={listaTipos[tipo]}
+								id="precio"
+								onChange={textRefFu}
+								required={true}
+								autoComplete="off"
+								type="text"
+								ref={textRef}
+							/>
+						</FormControl>
+					)}
+					<Box sx={{marginTop: 2, width: "13rem"}}>
+						<FormControl sx={{width: "13rem"}}>
+							<InputLabel>Tipo</InputLabel>
+							<Select
+								defaultValue={"ReleaseDate"}
+								onChange={(event) => {
+									addTipo(event.target.value)
+								}}
+								label="tipo"
+							>
+								<MenuItem value={"ReleaseDate"}>Release Date</MenuItem>
+								<MenuItem value={"title"}>Game title</MenuItem>
+								<MenuItem value={"protagonist"}>Protagonist</MenuItem>
+								<MenuItem value={"director"}>Director</MenuItem>
+							</Select>
+						</FormControl>
+					</Box>
+				</div>
+
+				<div style={{height: "55%", width: "100%"}}>
+					<DataGrid
+						getRowId={(row) => row.idGame}
+						rows={datos}
+						columns={columns}
+						pageSize={5}
+						rowsPerPageOptions={[5]}
 					/>
-				</FormControl>
-			) : (
-				<FormControl onChange={textRefFu} fullWidth>
-					<InputLabel>{tipo[0].toUpperCase() + tipo.substring(1)}</InputLabel>
-					<Input
-						placeholder={listaTipos[tipo]}
-						id="precio"
-						required={true}
-						autoComplete="off"
-						type="text"
-						ref={textRef}
-					/>
-				</FormControl>
-			)}
-			<Box sx={{marginTop: 2, width: "13rem"}}>
-				<FormControl sx={{width: "13rem"}}>
-					<InputLabel>Tipo</InputLabel>
-					<Select
-						defaultValue={"ReleaseDate"}
-						onChange={(event) => {
-							addTipo(event.target.value)
-						}}
-						label="tipo"
-					>
-						<MenuItem value={"ReleaseDate"}>Release Date</MenuItem>
-						<MenuItem value={"title"}>Game title</MenuItem>
-						<MenuItem value={"protagonist"}>Protagonist</MenuItem>
-						<MenuItem value={"director"}>Director</MenuItem>
-					</Select>
-				</FormControl>
-			</Box>
-		</>
+				</div>
+			</div>
+		</div>
 	)
 }
